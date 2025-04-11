47bd0a6121b4d851b98b7ca627ff99e0dfb9098a
mim-mim-ji <75061643+mim-mim-ji@users.noreply.github.com>
2025-04-11 10:43:16 +0900

    [Bronze III] Title: 소금 폭탄, Time: 168 ms, Memory: 17588 KB -BaekjoonHub

--- 백준/Bronze/13223. 소금 폭탄/소금 폭탄.java
+++ 백준/Bronze/13223. 소금 폭탄/소금 폭탄.java
@@ -1,35 +1,33 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] now = sc.nextLine().split(":");
         String[] after = sc.nextLine().split(":");
-
         System.out.println(solution(now, after));
     }
 
     private static String solution(String[] now, String[] after) {
-        int next = 0;
-        int[] result = new int[3];
+        int nowSec = toSecond(now);
+        int afterSec = toSecond(after);
+        int dif = afterSec - nowSec;
 
-        for (int i = 2; i > -1; i--) {
-            int dif = Integer.parseInt(after[i]) - Integer.parseInt(now[i]) - next;
-            next = 0;
-            if (dif < 0) {
-                if (i == 0)
-                    dif += 24;
-                else
-                    dif += 60;
-                next++;
-            }
-            result[i] = dif;
-        }
-        // 시, 분, 초가 모두 0이면 24:00:00으로 바꿔줌
-        if (result[0] == 0 && result[1] == 0 && result[2] == 0) {
-            result[0] = 24;
-        }
-        return String.format("%02d:%02d:%02d", result[0], result[1], result[2]);
+        if (dif <= 0)
+            dif += 24 * 60 * 60;
+
+        int h = dif / 3600;
+        int m = (dif % 3600) / 60;
+        int s = dif % 60;
+
+        return String.format("%02d:%02d:%02d", h, m, s);
     }
+
+    private static int toSecond(String[] time) {
+        int h = Integer.parseInt(time[0]);
+        int m = Integer.parseInt(time[1]);
+        int s = Integer.parseInt(time[2]);
+        return h * 3600 + m * 60 + s;
+    }
+
 }

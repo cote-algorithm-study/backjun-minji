38ab8e9aedf090b582c5bd30caebf844aca04e17
mim-mim-ji <75061643+mim-mim-ji@users.noreply.github.com>
2025-05-01 11:10:12 +0900

    [Silver V] Title: 단어 정렬, Time: 1448 ms, Memory: 28712 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -2,34 +2,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        
-        List<String> list = new ArrayList<>();
-
-        for (int i = 0; i < N; i++) {
-            String now = br.readLine();
-            if (!list.contains(now)) {
-                list.add(now);
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int N = Integer.parseInt(br.readLine());
+            List<String> words = new ArrayList<>();
+            for (int i = 0; i < N; i++) {
+                String s = br.readLine();
+                if (words.contains(s)) {
+                    continue;
+                }
+                words.add(s);
             }
-        }
 
-        Collections.sort(list, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
+            words.sort((o1, o2) -> {
                 if (o1.length() == o2.length()) {
                     return o1.compareTo(o2);
                 }
                 return o1.length() - o2.length();
-            }
-        });
+            });
 
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+            StringBuilder sb = new StringBuilder();
+            for (String word : words) {
+                sb.append(word).append("\n");
+            }
+            System.out.println(sb);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
+
 }

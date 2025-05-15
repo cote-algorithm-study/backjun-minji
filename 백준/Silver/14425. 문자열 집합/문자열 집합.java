3d2ab45f3933c18e439c3620752438bf334f07ad
mim-mim-ji <75061643+mim-mim-ji@users.noreply.github.com>
2025-05-15 17:57:56 +0900

    [Silver IV] Title: 문자열 집합, Time: 372 ms, Memory: 31192 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,28 +1,28 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
-    public static void main(String[] args) {
+       public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
 
-            String[] origin = new String[N];
+            Set<String> origin = new HashSet<>();
             for (int i = 0; i < N; i++) {
-                origin[i] = br.readLine();
+                origin.add(br.readLine());
             }
 
             int count = 0;
             for (int i = 0; i < M; i++) {
                 String s = br.readLine();
-                for (String ori : origin) {
-                    if (ori.equals(s))
-                        count++;
+                if(origin.contains(s)){
+                    count++;
                 }
             }
 
